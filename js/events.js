cd61101297d9f5192ba33becb036893a70e81c10
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-11-09 11:34:52 +0200

    dark mode

--- js/events.js
+++ js/events.js
@@ -8,5 +8,26 @@ function initTheme(tema) {
 initTheme('Events');
 
 // nusitaikyti i mygtuka
+const btn1El = document.getElementById('btn1');
+console.log('btn1El ===', btn1El);
 // uzdeti jam paspausimo pasiklausyma
+btn1El.addEventListener('click', sayPressed);
 // atspausdinti su console zodi 'paspaudei'
+
+function sayPressed() {
+  console.count('Paspaudei');
+  titleChange();
+}
+// sayPressed();
+console.log('sayPressed ===', sayPressed);
+
+const titleEl = document.getElementById('main-title');
+function titleChange() {
+  titleEl.style.fontSize = '50px';
+  titleEl.style.fontWeight = 'normal';
+}
+titleEl.addEventListener('mouseenter', titleChange);
+
+// nusitaikyti i mygtuka
+// pasirasyti veiksma atliekancia funkcija (document.body)
+// sukurti eventlisteneri
