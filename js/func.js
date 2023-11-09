c30ea331d830b4278bfd44b64da444ff7abd8be2
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-11-09 09:48:30 +0200

    func

--- js/func.js
+++ js/func.js
@@ -1,12 +1,13 @@
 'use strict';
 console.log('func.js file was loaded');
 
-const tema = 'Funkcijos ir Events';
-
-document.title = tema;
-const h1El = document.querySelector('h1');
-h1El.textContent = tema;
-
+function initTheme(tema) {
+  document.title = tema;
+  const h1El = document.querySelector('h1');
+  h1El.textContent = tema;
+}
+initTheme('Funkcijos ir Events');
+// initTheme('Apie kazka');
 // Functions
 /*
 funciton pavadinimas(param) {
@@ -20,10 +21,24 @@ function sayHi(userName = 'random person') {
   console.log(`Hello world from: ${userName}`);
 }
 
-sayHi('James');
-sayHi('Mike');
-sayHi();
-sayHi();
+// sayHi('James');
+// sayHi('Mike');
+// sayHi();
+// sayHi();
 
 // sukurti funkcija sum() turetu sudeti 2 skaicius ir atspausdinti rezutata
 // paduoti skaicius kaip argumentus iskvieciam sum(5, 10)
+// debugger;
+function sum(sk1, sk2) {
+  const suma = sk1 + sk2;
+  console.log(`${sk1} + ${sk2} = ${suma}`);
+  return suma;
+}
+
+let rez1 = sum(5, 15);
+let rez2 = sum(10, 50);
+// atspausdinti kokia yra situ sumu suma
+console.log('rez1 ===', rez1);
+console.log('rez2 ===', rez2);
+
+sum(rez1, rez2);
