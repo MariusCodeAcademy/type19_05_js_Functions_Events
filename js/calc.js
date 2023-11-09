3bfac1c81d8abd78ab1cb11eedff8a350c3318a3
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-11-09 10:41:17 +0200

    func

--- js/calc.js
+++ js/calc.js
@@ -9,5 +9,61 @@ function initTheme(tema) {
 initTheme('Paskaiciuoti');
 
 // sukurti funkcija kuri suskaiciuoja 3 skaiciu vidurki ir atspausdina avg(1,2,3)
+function avg(sk1, sk2, sk3) {
+  let localAvg = (sk1 + sk2 + sk3) / 3;
+  console.log('localAvg ===', localAvg.toFixed(2));
+  return localAvg;
+}
 // panaudoti ta funkcija 3 kartus
+let rez1 = avg(1, 2, 3);
+let rez2 = avg(4, 5, 6);
+let rez3 = avg(40, 54, 61);
 // norim gauti visu 3 funkciju rezultatu suma
+let suma = rez1 + rez2 + rez3;
+
+console.log('suma ===', suma);
+// parasyti funkcija kuri gauna 3 skaicius ir
+// jei nors vienas skaicius neigiamas, padaryti teigiamu
+// suskaiciuoti ir grazinti vidurki
+// posAvg(1,-2,-3) turetu grazinti 2
+
+// parasyti funkcija kuri spausdina skaicius nuo 0 iki 10
+// jei sk lygus 7 norim nutraukti funkcijo vykdyma
+// let i;
+// for (i = 0; i < 11; i++) {
+//   // break - nutraukia ciklo vykdyma
+//   // continue - persoka i step dali (i++)
+//   // patikrinima
+//   if (i === 7) {
+//     // break;
+//     continue;
+//   }
+//   console.log('i ===', i);
+// }
+// console.log('i po ciklo ===', i);
+
+// parasyti funkcija kuri spausdina skaicius nuo 0 iki 10
+// jei sk lygus 7 norim nutraukti funkcijo vykdyma
+
+function printNums() {
+  for (let i = 0; i < 11; i++) {
+    console.log('i ===', i);
+    if (i === 7) {
+      // break;
+      return;
+    }
+  }
+}
+// printNums();
+
+function printOnlyPositives(num1) {
+  // exit clause
+  if (typeof num1 === 'string') return;
+
+  if (num1 < 0) return;
+
+  console.log(`${num1} is a positive number`);
+}
+printOnlyPositives(5);
+printOnlyPositives(-15);
+printOnlyPositives('asd');
