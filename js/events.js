3a950b8977631a28b4411f0d82815e64d08f0d19
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-11-09 11:53:39 +0200

    dark toggle

--- js/events.js
+++ js/events.js
@@ -9,7 +9,7 @@ initTheme('Events');
 
 // nusitaikyti i mygtuka
 const btn1El = document.getElementById('btn1');
-console.log('btn1El ===', btn1El);
+// console.log('btn1El ===', btn1El);
 // uzdeti jam paspausimo pasiklausyma
 btn1El.addEventListener('click', sayPressed);
 // atspausdinti su console zodi 'paspaudei'
@@ -19,7 +19,7 @@ function sayPressed() {
   titleChange();
 }
 // sayPressed();
-console.log('sayPressed ===', sayPressed);
+// console.log('sayPressed ===', sayPressed);
 
 const titleEl = document.getElementById('main-title');
 function titleChange() {
@@ -29,5 +29,31 @@ function titleChange() {
 titleEl.addEventListener('mouseenter', titleChange);
 
 // nusitaikyti i mygtuka
+const btn2El = document.getElementById('btn2');
+console.log('btn2El ===', btn2El);
 // pasirasyti veiksma atliekancia funkcija (document.body)
+function darkModeOn() {
+  document.body.style.background = '#333';
+  document.body.style.color = 'white';
+}
+let isDarkModeOn = false;
+function darkModeToggle() {
+  if (isDarkModeOn === true) {
+    console.log('day mode on');
+    document.body.style.background = 'white';
+    document.body.style.color = '#333';
+    isDarkModeOn = false;
+  } else {
+    console.log('dark mode on');
+    document.body.style.background = '#333';
+    document.body.style.color = 'white';
+    isDarkModeOn = true;
+  }
+}
+// darkModeOn();
 // sukurti eventlisteneri
+btn2El.addEventListener('click', darkModeToggle);
+
+// padaryti kad mygukas veiktu kaip toggle
+//<button id="btn3">Turn <strong>on</strong> Dark mode</button>
+// paspaudi ir pasikeicia spalvos ir tekstas i <button id="btn3">Turn <strong>Off</strong> Dark mode</button>
