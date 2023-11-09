ff65d6a04f4ca71c82a2dd7d860ea3cbc47e7a43
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-11-09 09:08:26 +0200

    sum

--- js/func.js
+++ js/func.js
@@ -6,3 +6,24 @@ const tema = 'Funkcijos ir Events';
 document.title = tema;
 const h1El = document.querySelector('h1');
 h1El.textContent = tema;
+
+// Functions
+/*
+funciton pavadinimas(param) {
+  function body - vykdomas kai iskviesim funkcija
+}
+*/
+
+// deklaruojam funkcija - kazuo panasu i kintamojo deklaravima
+// deklaruojant funkcija nevykdoma tik isimenama
+function sayHi(userName = 'random person') {
+  console.log(`Hello world from: ${userName}`);
+}
+
+sayHi('James');
+sayHi('Mike');
+sayHi();
+sayHi();
+
+// sukurti funkcija sum() turetu sudeti 2 skaicius ir atspausdinti rezutata
+// paduoti skaicius kaip argumentus iskvieciam sum(5, 10)
